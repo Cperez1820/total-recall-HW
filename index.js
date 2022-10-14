f200ed90aa6188eabbb5a24aee07abfc86edbe1b
chris p <christopherperez1820@gmail.com>
2022-10-14 19:50:49 -0400

    Hw commmit

--- index.js
+++ index.js
@@ -330,38 +330,38 @@
 // E. Mix It UP ==============================================================================================
 
 // Given the following array: 
-const myArray = [5, 10, 500, 20]
+// const myArray = [5, 10, 500, 20]
 
 //1. Add the string "Aegon"to the end of the array. Add another string of your choice to the end of the array.
 
-//ANSWER
-myArray[myArray.length] = "Aegon";
-myArray[myArray.length] = "Conqueror";
-    console.log(myArray)
+// //ANSWER
+// myArray[myArray.length] = "Aegon";
+// myArray[myArray.length] = "Conqueror";
+//     console.log(myArray)
 
 //2. Remove the 5from the beginning of the array.
 
-//ANSWER
-myArray.shift(myArray[0])
-    console.log(myArray)
+// //ANSWER
+// myArray.shift(myArray[0])
+//     console.log(myArray)
 
 //3. Add the string "Bob Marley"to the beginning of the array.
 
-//ANSWER
-myArray.unshift("Bob Marley");
-    console.log(myArray)
+// //ANSWER
+// myArray.unshift("Bob Marley");
+//     console.log(myArray)
 
 //4. Remove the string of your choice from the end of the array.
 
-//ANSWER
-myArray.pop(5)
-    console.log(myArray)
+// //ANSWER
+// myArray.pop(5)
+//     console.log(myArray)
 
 //5a. Reverse this array using Array.prototype.reverse(). 
 
-//ANSWER
-myArray.reverse();
-    console.log(myArray)
+// //ANSWER
+// myArray.reverse();
+//     console.log(myArray)
 
 //5b. Did you mutate the array? What does mutate mean? Did the .reverse()method return anything?
 
@@ -375,8 +375,356 @@ myArray.reverse();
 // Write an if ... elsestatement that:
 
 //1. console.log()s "little number" if the number is entered is less than 100.
-
-
-
 //2. console.log()s big numberif the number is greater than or equal to 100.
+
+//ANSWER TO BOTH BELOW
+// let num = 120
+// if ( num < 100) {
+//     send = "little number";
+// }else if( num >= 100 ){
+//     send = "big number"
+// }else {
+//     send = "number"
+// }
+// console.log(send)
+
+
+// G. Monkey in the Middle ======================================================================================
+
+
+//1. console.log()little numberif the number entered is less than 5.
+//2. If the number entered is more than 10, log big number.
+//3. Otherwise, log "monkey".
+
+//ANSWER TO ALL THREE BELOW
+// let mon = 7
+// if ( mon < 5 ){
+//     send = "little number";
+// }else if( mon > 10 ){
+//     send = "big number"
+// }else {
+//     send = "monkey"
+// }
+// console.log(send)
+
+//H. What's in Your Clost? =====================================================================================
+
+// const kristynsCloset = [
+//     "left shoe",
+//     "cowboy boots",
+//     "right sock",
+//     "GA hoodie",
+//     "green pants",
+//     "yellow knit hat",
+//     "marshmallow peeps"
+//   ];
+  
+  // Thom's closet is more complicated. Check out this nested data structure!!
+//   const thomsCloset = [
+//     [
+//       // These are Thom's shirts
+//       "grey button-up",
+//       "dark grey button-up",
+//       "light blue button-up",
+//       "blue button-up",
+//     ],[
+//       // These are Thom's pants
+//       "grey jeans",
+//       "jeans",
+//       "PJs"
+//     ],[
+//       // Thom's accessories
+//       "wool mittens",
+//       "wool scarf",
+//       "raybans"
+//     ]
+//   ];
+
+//1. What's Kristyn wearing today? Using bracket notation to access items in kristynsCloset,
+//  log the sentence "Kristyn is rocking that " + the third item in Kristyn's closet + " today!" to the console.
+
+// kristynsCloset.pop([3])
+//   console.log("kristyn is rocking that " + kristynsCloset[2] + " today")
+
+//2. Kristyn just bought some sweet shades! Add "raybans"to her closet after "yellow knit hat".
+
+// kristynsCloset.push("raybans")
+//   console.log(kristynsCloset)
+
+//3. Kristyn spilled coffee on her hat... modify this item to read "stained knit hat"instead of yellow.
+
+// //ANSWER
+// kristynsCloset[5] = "stained knit hat"
+
+//   console.log(kristynsCloset)
+
+
+//4. Put together an outfit for Thom! Using bracket notation, access the first element in Thom's shirtsarray.
+
+//ANSWER
+// let shirt = thomsCloset[0][0]
+//   console.log(shirt)
+
+//5. In the same way, access one item from Thom's pants array.
+
+//ANSWER
+// let pants = thomsCloset[1][1]
+//   console.log(pants)
+
+//6. Access one item from Thom's accessories array.
+
+//ANSWER
+// let misc = thomsCloset[2][2]
+//   console.log(misc)
+
+//7. Log a sentence about what Thom's wearing. Example: "Thom is looking fierce in a grey button-up, jeans and wool scarf!"
+
+// //ANSWER
+// console.log("Thom is looking fierce in " + shirt + ", " + pants + " and " + misc)
+
+//8. Get more specific about what kind of PJs Thom's wearing this winter. Modify the name of his PJ pants to Footie Pajamas.
+
+// //ANSWER  , even tho i dont really understand this one to much 
+// kristynsCloset[1][2] = "Footie Pajamas"
+//   console.log(thomsCloset)
+
+//==================================== IV. Functions ========================================================================
+
+//A. printGreeting =================================================================================================
+
+// lol skipped cuz i read it :D
+
+//B. printCool =====================================================================================================
+
+// Write a function printCool that accepts one parameter, name as an argument. The function should print the name and a message saying that that person is cool.
+
+//ANSWER 
+
+// const printCool = (argument) => {
+//    return argument + " is cool"; 
+// };
+//   console.log(printCool("Captain Reynolds"));
+
+//C. calculateCube =================================================================================================
+
+// Write a function calculateCube that takes a single number and prints the volume of a cube made from that number.
+
+//ANSWER 
+// const calculateCube = (cubed) => {
+//     return  "12" + cubed;
+// }
+//   console.log(calculateCube(5));
+
+//D. isVowel =======================================================================================================
+
+// Write a function isVowelthat takes a character (i.e. a string of length 1) and returns true if it is a vowel,
+// false otherwise. The vowel could be upper or lower case. Test your function on every vowel and make sure it's working.
+// In general, when you write functions, take a minute to test them with different values to make sure they behave the way you want.
+
+//ANSWER 
+// const isAVowel = (character) => {
+// 	if (character === "a" || character === "e" || character === "i" || character === "o" || character === "u"){
+// 		return true;
+// 	} else {
+// 		return false;
+// 	}
+// };
+// console.log(isAVowel("A".toLowerCase()));
+
+//E. getTwoLengths ===================================================================================================
+
+
+// Write a function getTwoLengths that accepts two parameters (strings).
+//  The function should return an array of numbers where each number is the length of the corresponding string.
+
+//ANWER
+// let getTwoLengths = (string1, string2) => {
+// 	const lengArray = [string1.length, string2.length];
+// 	return lengArray;
+  
+// };
+
+// //=> [4, 13]
+
+
+
+// console.log(getTwoLengths("Hank", "Hippopopalous"));
+
+//F. getMultipleLengths ==========================================================================================
+
+
+// Write a function getMultipleLengthsthat accepts a single parameter as an argument: an array of strings.
+//  The function should return an array of numbers where each number is the length of the corresponding string.
+
+// //ANSWER
+// const getMultipleLengths = (stringArr) => {
+//     let multipleLengths = [];
+//         for( x = 0; x < stringArr.length;x++) {
+//             multipleLengths.push(stringArr[x].length);
+//           };
+//     return multipleLengths;
+
+// };
+// console.log(getMultipleLengths(["hello", "what", "is", "up", "dude"]))
+
+//G. maxOfThree =====================================================================================================
+
+
+// //ANSWER THIS ONE IS FUNNY i had <= in the for loop after like ten mins of intense staring i noticed and had a good laugh clg kept giving me princess
+// let printLongestWord = (wordArr) => {
+//     let lWord = "";
+//         for(x = 0; x < wordArr.length;x++){
+//               if(wordArr[x].length > lWord.length){
+//                  lWord = wordArr[x];
+//            };
+
+
+//         };
+//         return lWord;
+// };
+// console.log(printLongestWord(["BoJack", "Princess", "Diane", "a", "Max", "Peanutbutter", "big", "Todd"]));
+
+//========================================= OBJECTS ================================================================
+
+// A. Make a user object ===============================================================================================
+
+//  const user = {name: "Billy", email: "billy1922@gmail.com", age: 90, purchased: []}; {
+ 
+//  }
+//  //console.log(user)
+// //B. Update the user ========================================================================================================
+
+//  //1.
+//  user.email = "Billyrisesagain@gmail.com";
+//   //  console.log(user.email)
+
+// / //2.
+// user.age++;
+// //   console.log(user)
+
+// //C. Adding keys and values ===================================================================================================
+
+// // //3. 
+// // user.location = ("New York")
+
+// //D. Shopalic! =======================================================================================================================
+
+// //4abc.
+// user.purchased.push("carbohydrates" , "peace of mind" , "Merino jodhpurs");
+
+// // //4d. 
+// // console.log(user.purchased[2])
+
+// //E. OBJECT-WITHIN-OBJECT =======================================================================================================
+
+// //1.
+// user.friend = {
+//   name:"Jimmy John",
+//   age: 26,
+//   location: "manhattan",
+//   purchased: []
+// }
+
+// //2.
+// // console.log(user.friend.name)
+
+// //3.
+// // console.log(user.friend.location)
+
+// //4.
+// user.friend.age = ("55");
+// // console.log(user.friend)
+
+// //5. 
+// user.friend.purchased.push("The One Ring", "A latte")
+
+// //6. 
+// // console.log(user.friend.purchased[1])
+
+// // F. Loops ====================================================================================================================================
+
+// //answer 
+// function listUser(listParam) {
+//   listParam.purchased.forEach((x) => {
+//       // console.log(x)
+//   })
+// }
+
+
+
+// function listPeople(listParam) {
+//   listParam.friend.purchased.forEach((x) => {
+//       // console.log(x)
+//   })
+// }
+
+// // console.log(user.purchased)  // <----- 1.
+// // console.log(user.friend.purchased) // <---- 2.
+
+
+
+// //intial thought
+// // for( x = 0; x < 2;x++)
+// //     if(x < user.purchased){
+
+// //     }
+
+    
+
+// //     console.log(x)
+
+
+// // G.===================================================================================================================
+
+
+// //ANSWER 1. 2. 3.
+// // function updateUser() {
+// //     user.age++
+// //     user.name = user.name.toUpperCase();
+// // }
+// // updateUser()
+// //   console.log(user)
+
+
+// //ANSWER 
+
+// function outAndLoud(person) {
+//     person.age++
+//     person.name = person.name.toUpperCase();
+// };
+// outAndLoud(user)
+// outAndLoud(user.friend)
+//   console.log(user)
+
+  //=================================== Cat Combinator =====================================================================================================
+
+//ANSWER 1.
+let cat1 = {
+  name: "Whiskers",
+  breed: "ragdoll",
+  age: "6",
+}
+console.log(cat1.age)
+console.log(cat1.breed)
+
+//ANSWER 2.
+
+let cat2 = {
+  name: "Jolt",
+  breed:"birman",
+  age: 2,
+}
+
+//ANSWER 3.
+function combineCats(mama , papa) {
+  return {
+    name: mama.name + papa.name,
+      age: 1,
+    breed: mama.breed + "-" + papa.breed,
+  }
+}
+//console.log(combineCats(cat1, cat2));
+
+//ANSWER 4. Why just WHY.
+console.log(combineCats(combineCats(combineCats(cat1, cat2), combineCats(combineCats(cat1, cat2), combineCats(cat1, cat2))),combineCats(cat1, cat2)));
 
